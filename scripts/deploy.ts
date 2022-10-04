d0c32e77dc755ceb67cbfe925d7fc658518a3acf
CoviloMilos <miloscovilons@gmail.com>
2022-10-04 21:26:48 +0200

    Cleanup for week2 task

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,15 +1,6 @@
 import { ethers } from "hardhat";
 
-async function main() {
-  const factory = await ethers.getContractFactory("MVPWAirlines");
-  const contract = await factory.deploy(
-    "0x71bDd3e52B3E4C154cF14f380719152fd00362E7"
-  );
-
-  await contract.deployed();
-
-  console.log("MVPWAirlines deployed to: ", contract.address);
-}
+async function main() {}
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
